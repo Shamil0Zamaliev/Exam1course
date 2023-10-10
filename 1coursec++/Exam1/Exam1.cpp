dd53bf62b97c1aabfc82225bcc633d221af81b9c
Shamil0Zamaliev <114811430+Shamil0Zamaliev@users.noreply.github.com>
2023-10-10 09:24:28 +0300

    Add files via upload

---
+++ 1coursec++/Exam1/Exam1.cpp
@@ -0,0 +1,102 @@
+﻿// Exam1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <fstream>
+
+
+template<typename T>
+T getRandomNumber(T min, T max)
+{
+	T value;
+	value = rand() % (int)pow(10, 2);
+	value = min + (value / pow(10, 2)) * (max - min);
+
+	return value;
+}
+
+template<class T>
+void printMat(std::vector<std::vector<T>> const& mat) {
+	std::cout << '\n' << "Current matrix" << '\n';
+	for (std::vector<T> row : mat)
+	{
+		for (T val : row) {
+			std::cout << val << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+template<class T>
+void sumOfRows(std::vector<std::vector<T>> const& mat) {
+	int i = 1;
+	for (std::vector<T> row : mat)
+	{
+		T res{ 0 };
+		for (T element : row) {
+			res += element;
+		}
+		std::cout << "Sum of row #" << i << " = " << res << std::endl;
+		++i;
+	}
+}
+template <class T>
+std::vector<std::vector<T>> readMatrixFromFile(const std::string& filename, int rows, int cols) {
+	std::ifstream file(filename, std::ios::binary);
+	std::vector<std::vector<T>> matrix(rows, std::vector<T>(cols));
+
+	if (file.is_open()) {
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; j++)
+				file.read(reinterpret_cast<char*>(&matrix[i][j]), sizeof(T));
+	}
+	file.close();
+
+
+	return matrix;
+}
+
+int main() {
+	srand(time(0));
+	std::cout << "Enter dimnesion (M x N): " << '\n';
+	int M;
+	int N;
+	std::cin >> M >> N;
+
+	std::vector<std::vector<double>> temp(M, std::vector<double>(N));
+
+	//заполняем массив
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			temp[i][j] = getRandomNumber(0.0, 10.0);
+		}
+
+	}
+
+
+
+	std::ofstream fout("ex1.txt", std::ios::out | std::ios::binary); //создаем и открываем файл на запись в бинарном режиме
+
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			fout.write((char*)&temp[i][j], sizeof(double));  //записываем в файл массив
+		}
+
+	}
+	fout.close();
+	printMat(temp);
+
+
+
+	std::vector<std::vector<double>> matrix = readMatrixFromFile<double>("ex1.txt", M, N);
+	printMat(matrix);
+	sumOfRows(matrix);
+
+}
---
+++ 1coursec++/Exam2/Exam2.cpp
@@ -0,0 +1,100 @@
+﻿// Exam2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+
+
+
+std::string replace(const std::string &line) {
+    std::string res;
+
+    for (char c : line) {
+        if (std::isdigit(c)) {
+            switch (c) {
+                case '0':
+                    res += "zero";
+                    break;
+                case '1':
+                    res += "one";
+                    break;
+                case '2':
+                    res += "two";
+                    break;
+                case '3':
+                    res += "three";
+                    break;
+                case '4':
+                    res += "four";
+                    break;
+                case '5':
+                    res += "five";
+                    break;
+                case '6':
+                    res += "six";
+                    break;
+                case '7':
+                    res += "seven";
+                    break;
+                case '8':
+                    res += "eight";
+                    break;
+                case '9':
+                    res += "nine";
+                    break;
+                default:
+                    res += c;
+                    break;
+            }
+        }
+        else
+            res += c;
+    }
+
+    return res;
+}
+
+void Print(const std::string &text) {
+    bool newSentence = false;
+
+    for (char c : text) {
+        std::cout << c;
+        if (c == '.' || c == '!' || c == '?') {
+            newSentence = true;
+        }
+        if (newSentence && c != ' ' && c != '\n') {
+            newSentence = false;
+            std::cout << std::endl;
+        }
+    }
+}
+
+void filter(const std::string& filename) {
+    std::ifstream fin(filename);
+
+    if (!fin) {
+        std::cout << "Failed to open file: " << filename << '\n';
+        return;
+    }
+    std::string line;
+    std::string text;
+
+    while (std::getline(fin, line)) {
+        text += replace(line) + '\n';
+    }
+    fin.close();
+
+    Print(text);
+}
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::string path = "test.txt.txt";
+
+    filter(path);
+}
